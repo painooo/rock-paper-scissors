a05899ca0d683beb40d7b08da23d32cf0800def3
pyr <jzhangnv@gmail.com>
2025-12-14 16:25:14 -0500

    Added index.html & script.js to start project

---
+++ script.js
@@ -0,0 +1,20 @@
+// Rock-Paper-Scissor game
+// A. Get user input rock, paper, or scissor
+// B. Convert user option to number for easier managemnet
+// C. Get computer options from 0-2
+// D. rock = 0, paper = 1, scissor = 2
+// E. Subtract num by each other
+// E1. If negative computer wins, If positive user wins
+// E2. If == 0 then tie
+/* 
+Comp: left, User: right
+rock v paper; 0 v 1 : -1 (0 - 1)
+rock v scissor; 0 v 2 : -2
+paper v scissor; 1 v 2: -1
+Comp: right, User, left
+1
+2
+1
+*/
+
+console.log('abc')
