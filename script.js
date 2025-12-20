78c3f1887db153f19e3490d037d1f43b042d3639
pyr <jzhangnv@gmail.com>
2025-12-20 11:21:41 -0500

    Used switchcase with buttons for user input + textContent to display user score

--- script.js
+++ script.js
@@ -3,27 +3,28 @@
 let userScore = 0;
 let compScore = 0;
 const options = document.querySelector("#options");
+const userScoreInput = document.querySelector("#userScore");
+const compScoreInput = document.querySelector("#compScore");
+const victor = document.querySelector("#victor");
 options.addEventListener("click", (e) => {
-    console.log((e.target)['id']);
-    // add switch case here for user play
-});
-
-function userPlay(userPrompt) {
-    let options = ["rock","paper","scissor"];
-    if (options.includes(userPrompt.toLowerCase())) {
-        return options.indexOf(userPrompt);
+    switch((e.target)['id']){
+        case('rock'):
+            playGame(1);
+            break;
+        case('paper'):
+            playGame(2);
+            break;
+        case('scissor'):
+            playGame(3);
+            break;
     }
-}
+});
 function computerPlay(){
     return Math.floor(Math.random()*3)
 }
-function playGame(rounds) {
-while (rounds != 0) {
-    let userPrompt = prompt("Rock, Paper, or Scissor?");
-    let user = userPlay(userPrompt);
+function playGame(user) {
     let computer = computerPlay();
     let result = computer - user;
-
     if (result == -2 || result == 1) {
         console.log("Computer wins")
         updScore(3, "computer")
@@ -35,9 +36,9 @@ while (rounds != 0) {
     }
     console.log("User score: "+userScore);
     console.log("Computer score: "+compScore);
-    console.log("Rounds left: "+rounds)
-    rounds--;
-}
+    userScoreInput.textContent = `User score: ${userScore}`;
+    compScoreInput.textContent = `Computer score: ${compScore}`
+    
 }
 function updScore(score, player) {
     if (player === "user") {
@@ -46,5 +47,3 @@ function updScore(score, player) {
         compScore += score;
     }
 }
-let rounds = parseInt(prompt("How many rounds do you want to play?"));
-playGame(rounds);
